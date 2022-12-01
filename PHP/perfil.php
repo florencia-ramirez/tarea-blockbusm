4292de9d614f87f061ec3bf2b92ad94ce1e7b801
ScaryShiroyasha <68254445+ScaryShiroyasha@users.noreply.github.com>
2022-12-01 18:13:23 -0300

    probando bootstrap

--- PHP/perfil.php
+++ PHP/perfil.php
@@ -5,14 +5,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BLOCKBUSM | Tu cuenta</title>
+    <link rel="stylesheet" href="./CSS/bootstrap.min.css" />
+    <title>BLOCKBUSM</title>
 </head>
 
 <body>
-    <h1>Este es tu perfil</h1>
-    <!-- Mostrar nombre, usuario, maybe edad pa las categorias ? -->
-    <!-- Mostrar wishlist -->
-    <!-- Mostrar peliculas rentadas ?? -->
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nav 1</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nav 2</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="jumbotron">
+                <h1 class="display-3">Jumbo heading</h1>
+                <p class="lead">Jumbo helper text</p>
+                <hr class="my-2">
+                <p>More info</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                </p>
+            </div>
+
+
+        </div>
+    </div>
 </body>
 
 </html>
