f96548fcbb3fd34c797f22a99107963593e06ff2
florencia-ramirez <florencia.ramirez@alumnos.inf.utfsm.cl>
2022-12-04 15:02:48 -0300

    weas varias

--- PHP/peliculas.php
+++ PHP/peliculas.php
@@ -2,30 +2,35 @@
 include("cabeza.php");
 include("conexion-bd.php");
 $sql = "SELECT * FROM peliculas";
-if($result = mysqli_query($conexion, $sql)){
-    if(mysqli_num_rows($result) > 0){
-        while($row = mysqli_fetch_array($result)){
+if ($result = mysqli_query($conexion, $sql)) {
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_array($result)) {
 ?>
-
-<div class="col-md-3">
-    <div class="card border-primary mb-3">
-        <img class="card-img-top" src="<?php echo($row['imagen']);?>" alt="" style="height: auto; display: block; max-width: 100%;">
-        <div class="card-body">
-            <h4 class="card-title"><?php echo($row['nombre']); ?></h4>
-            <h6 class="card-text"><?php echo($row['genero']); ?></h6>           
-        </div>
-    </div>
-    <br/>
-</div>
+            <!-- Arreglar diferencias de tamaños, maybe el texto es muy garnde ?? -->
+            <div class="col-md-3">
+                <div class="card border-primary mb-3">
+                    <img class="card-img-top" src="<?php echo ($row['imagen']); ?>" alt="" style="height: auto; display: block; max-width: 100%;">
+                    <div class="card-body">
+                        <h4 class="card-title"><?php echo ($row['nombre']); ?></h4>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php echo ($row['genero']), " | ", ($row['duracion']), " minutos"; ?></h6>
+                        <p class="card-text"><?php echo ($row['descripcion']); ?></p>
+                        <p class="card-subtitle mb-2 text-muted"><?php echo "Reparto: ", ($row['reparto']); ?></p>
+                        <a type="button" class="btn btn-danger" href="favoritos.php"> <img src="../IMAGES/heart.svg" alt="" width="16" height="16"> </a>
+                        <a type="button" class="btn btn-danger" href="wishlist.php"> <img src="../IMAGES/bookmark.svg" alt="" width="16" height="16"> </a>
+                        <a type="button" class="btn btn-danger" href="rentar.php"> <img src="../IMAGES/shopping-cart.svg" alt="" width="16" height="16"> </a>
+                    </div>
+                </div>
+                <br />
+            </div>
 
 <?php   }
-        mysqli_free_result($result); 
-    } else{
+        mysqli_free_result($result);
+    } else {
         echo "No records matching your query were found.";
     }
-} else{
+} else {
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($conexion);
-}?>
+} ?>
 
 
 
--- PHP/perfil.php
+++ PHP/perfil.php
@@ -15,39 +15,32 @@ $sqlsentence = mysqli_query($conexion, "SELECT nombre FROM usuarios WHERE mail =
 $nombre = mysqli_fetch_column($sqlsentence);
 $sqlsentence = mysqli_query($conexion, "SELECT descripcion FROM usuarios WHERE mail = '$mail'");
 $descri = mysqli_fetch_column($sqlsentence);
+$sqlsentence = mysqli_query($conexion, "SELECT seguidos FROM usuarios WHERE mail = '$mail'");
+$seguido = mysqli_fetch_column($sqlsentence);
+$sqlsentence = mysqli_query($conexion, "SELECT seguidores FROM usuarios WHERE mail = '$mail'");
+$seguidor = mysqli_fetch_column($sqlsentence);
 
-
+// Reseñas
+$cantr = mysqli_num_rows(mysqli_query($conexion, "SELECT * FROM reseñas WHERE usuario = '$mail'"));
 
 ?>
 <div class="card mb-3">
 	<h2 class="card-header">Tu Perfil</h2>
+</br>
+	<img src="../IMAGES/user.svg" alt="icono" width="10%" height="25%">
 	<div class="card-body">
-		<h3 class="card-title"><?php echo $nombre ?></h3>
-	</div>
-	<img src="../IMAGES/blank-profile-picture.png" alt="icono" width="10%" height="25%">
-	<div class="card-body">
+		<h3 class="card-title"> <?php echo $nombre ?></h3>
+		<h6 class="card-subtitle mb-2 text-muted"><?php echo "Seguidores: ", $seguidor ?> | <?php echo "Seguidos: ", $seguido ?></h6>
 		<p class="card-text"><?php echo $descri ?></p>
-	</div>
-	<ul class="list-group list-group-flush">
-		<li class="list-group-item">Cras justo odio</li>
-		<li class="list-group-item">Dapibus ac facilisis in</li>
-		<li class="list-group-item">Vestibulum at eros</li>
-	</ul>
-	<div class="card-body">
 		<a href="#" class="card-link">Editar Cuenta</a>
 		<a href="#" class="card-link">Another link</a>
-	</div>
-	<div class="card-footer text-muted">
-		2 days ago
 	</div>
 </div>
 <div class="card">
 	<div class="card-body">
-		<h4 class="card-title">Card title</h4>
-		<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+		<h4 class="card-title">Reseñas</h4>
+		<h6 class="card-subtitle mb-2 text-muted"><?php echo "Total de reseñas hechas: ", $cantr ?></h6>
 		<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-		<a href="#" class="card-link">Card link</a>
-		<a href="#" class="card-link">Another link</a>
 	</div>
 </div>
 
